4ee863bb80ca820446ee2cb7eed8843bb96215c5
Maveyyl <martialveyry@hotmail.fr>
2016-07-29 17:13:37 +0200

    Work on q learning experiment 2

--- app/QLearningExperiment2/HexcerMap.js
+++ app/QLearningExperiment2/HexcerMap.js
@@ -33,23 +33,71 @@ HexcerMap.prototype.init = function(){
 	};
 }
 
-HexcerMap.prototype.is_wall = function(x, y){
-	return this.map[y][x] === constants.tiles.wall;
+
+HexcerMap.prototype.is_wall = function(pos){
+	return 
+	this.map[pos.y][pos.x] === constants.tiles.wall ||
+	(
+		pos.x < 0 || pos.x > this.size[0] ||
+		pos.y < 0 || pos.y > this.size[1]
+	);
 };
-HexcerMap.prototype.is_left_goal = function(x, y){
-	return this.left_goal.x === x && this.left_goal.y === y;
+HexcerMap.prototype.is_left_goal = function(pos){
+	return this.left_goal.x === pos.x && this.left_goal.y === pos.y;
 };
-HexcerMap.prototype.is_right_goal = function(x, y){
-	return this.right_goal.x === x && this.right_goal.y === y;
+HexcerMap.prototype.is_right_goal = function(pos){
+	return this.right_goal.x === pos.x && this.right_goal.y === pos.y;
 };
-HexcerMap.prototype.is_left_player = function(x, y){
-	return this.left_player.x === x && this.left_player.y === y;
+HexcerMap.prototype.is_left_player = function(pos){
+	return this.left_player.x === pos.x && this.left_player.y === pos.y;
 };
-HexcerMap.prototype.is_right_player = function(x, y){
-	return this.right_goal.x === x && this.right_goal.y === y;
+HexcerMap.prototype.is_right_player = function(pos){
+	return this.right_goal.x === pos.x && this.right_goal.y === pos.y;
 };
-HexcerMap.prototype.is_ball = function(x, y){
-	return this.ball.x === x && this.ball.y === y;
+HexcerMap.prototype.is_ball = function(pos){
+	return this.ball.x === pos.x && this.ball.y === pos.y;
+};
+HexcerMap.prototype.is_empty = function(pos){
+	return 
+	!this.is_wall(pos) && 
+	!this.is_left_player(pos) && 
+	!this.is_right_player(pos);
+};
+HexcerMap.prototype.is_left_player_winning = function(){
+	return this.ball.x === this.right_goal.x && this.ball.y === this.right_goal.y ;
+};
+HexcerMap.prototype.is_right_player_winning = function(){
+	return this.ball.x === this.left_goal.x && this.ball.y === this.left_goal.y ;
+};
+HexcerMap.prototype.get_neighbour = function(pos, direction){
+	var x = pos.x;
+	var y = pos.y;
+
+	switch(direction){
+		case constants.directions.left:
+			x--;
+			break;
+		case constants.directions.top_left:
+			x--;
+			y--;
+			break;
+		case constants.directions.top_right:
+			x++;
+			y--;
+			break;
+		case constants.directions.right:
+			x++;
+			break;
+		case constants.directions.bottom_right:
+			x++;
+			y++;
+			break;
+		case constants.directions.breakbottom_left:
+			x--;
+			y++;
+			break;
+	}
+	return {x:x, y:y};
 };
 
 HexcerMap.prototype.get_map = function(){
@@ -141,18 +189,60 @@ HexcerMap.prototype.apply_actions = function(left_action, right_action ){
 	right_action = ( right_action + constants.actions.no_move/2) % constants.actions.no_move;
 	
 	// compute future new positions of the players
+	var left_player_new_pos = this.get_neighbour( this.left_player, left_action );
+	if( this.is_empty( left_player_new_pos ) )
+		var left_player_new_pos = this.left_player;
+	var right_player_new_pos = this.get_neighbour( this.right_player, right_action );
+	if( this.is_empty( right_player_new_pos ) )
+		var right_player_new_pos = this.right_player;
+
 
 	// if new left position and new right position are the same
+	if( left_player_new_pos.x === right_player_new_pos.x && left_player_new_pos.y === right_player_new_pos.y ){
 		// compute which one prevails randomly
-
-		// set back the loosing player to its original position
+		var new_pos = left_player_new_pos;
+		var r = Math.random();
 
 		// if the ball was in one of the previous positions
+		if( 
+			( this.ball.x === this.left_player.x && this.ball.y === this.right_player.y ) ||
+			( this.ball.x === this.right_player.x && this.ball.y === this.right_player.y ) 
+		){
 			// set it to the new position
+			this.ball.x = new_pos.x;
+			this.ball.y = new_pos.y;
+		}
+
+		// set the winning player to its new position
+		if( r < 0.5 ){
+			this.left_player.x = new_pos.x;
+			this.left_player.y = new_pos.y;
+		}
+		else{
+			this.right_player.x = new_pos.x;
+			this.right_player.y = new_pos.y;
+		}
+	}
 	// if new left position and new right position are not the same position
+	else{		
 		// if ball was in previous left position
+		if(  this.ball.x === this.left_player.x && this.ball.y === this.right_player.y ) {
 			// set it to new left position
+			this.ball.x = left_player_new_pos.x;
+			this.ball.y = left_player_new_pos.y;
+		}
 		// if ball was in previous right position
+		else if( this.ball.x === this.right_player.x && this.ball.y === this.right_player.y ) {
 			// set it to new right position
+			this.ball.x = right_player_new_pos.x;
+			this.ball.y = right_player_new_pos.y;
+		}
+
+		// set players to their new positions
+		this.left_player.x = left_player_new_pos.x;
+		this.left_player.y = left_player_new_pos.y;
+		this.right_player.x = right_player_new_pos.x;
+		this.right_player.y = right_player_new_pos.y;
+	}
 
 };
